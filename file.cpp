072ae36f198bb8999340db9d30e5223ab57d5034
Faiz Ahmed <faizcse28@gmail.com>
2018-04-19 01:04:46 +0600

    data add in structur

--- file.cpp
+++ file.cpp
@@ -1,16 +1,20 @@
+#include <iostream>
 #include <string>
 #include<fstream>
 using namespace std;
-	
+
+
 int positionInList = 0;
 int listSize = 0;
 struct list
 {
-   string name;
+	string name;
    string description;
    string Date;
 };
 struct list data[100];
+
+
 
 bool Add_in_file(string name,string description, string Date)
 {
@@ -26,6 +30,42 @@ bool Add_in_file(string name,string description, string Date)
    {
       return false;
    }
+}
+
+
+void Show_file()
+{
+	ofstream file_open("To_Do_list.txt",ios::app);
+   for (int i = 0; i < listSize; i++)
+   {
+        file_open<<i+1<<endl;
+   	  file_open<<data[i].name<<endl;
+   	  file_open<<data[i].description<<endl;
+   	  file_open<<data[i].Date<<endl;
+   	  file_open<<"\n"<<endl;
+        cout<<i+1<<endl;
+   	  cout<<"Name : "<<data[i].name<<endl;
+      cout <<"Description : "<<data[i].description << endl;
+      cout <<"Date : "<< data[i].Date << endl;
+      cout << "" << endl;
+   }
+}
+void Delete(int x)
+{
+	ofstream file_open("To_Do_list.txt",ios::app);
+	file_open<<"Delete "<<x+1<<" positionInList "<<endl;
+   file_open<<"\n"<<endl;
+	for(int i=0;i<listSize;i++)
+	{
+		if(x<i)
+		{
+			data[i-1].name=data[i].name;
+			data[i-1].description=data[i].description;
+			data[i-1].Date=data[i].Date;
+		}
+	}
+	listSize-=1;
+
 }
 
 int main()
